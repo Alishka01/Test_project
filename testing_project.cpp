36b2961afbfcd3aee3f558dce4c6b89800fc4de5
Alishka01 <baryalin100@gmail.com>
2020-07-27 11:30:02 +0300

    Add cycle for()

--- testing_project.cpp
+++ testing_project.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
 using namespace std;
-int main () {
-cout << "Hello, friends" << endl;
 
-    return 0;
+int main () {
+cout << "начало цикла" << endl;
+
+for (int i = 0; i < 10; i++){
+    cout << "переменная i =  " << i << endl;
+}
+
+cout << "конец цикла" << endl;
+return 0;
 }
